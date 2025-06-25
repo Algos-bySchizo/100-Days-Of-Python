456adadb92d3ad60c95205f16847d87ec1544cb6
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-06-25 19:47:08 +0500

    applied the dunder method  __rerp__() for displaying the question bank in readable form rather than memory addresses

--- Day-17/question_model.py
+++ Day-17/question_model.py
@@ -2,3 +2,5 @@ class Question:
     def __init__(self, q_text, q_answer):
         self.text=q_text
         self.answer=q_answer
+    def __repr__(self):
+        return f"Question : {self.text} | Answer : {self.answer}\n"
