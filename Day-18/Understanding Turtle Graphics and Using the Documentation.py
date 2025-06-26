4e3e58eac810f4483f29370473f86eafd4025c9d
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-06-26 23:34:10 +0500

    updates

---
+++ Day-18/Understanding Turtle Graphics and Using the Documentation.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+jd_the_turtle=Turtle()
+jd_the_turtle.shape('turtle')
+jd_the_turtle.color('red')
+jd_the_turtle.forward(100)
+jd_the_turtle.right(90)
+screen=Screen()
+screen.exitonclick()
---
+++ Day-18/day-18-1.py
@@ -0,0 +1,17 @@
+# from turtle import *
+# jd=Turtle()
+# for i in range(4):
+#     jd.forward(100)
+#     jd.right(90)
+
+# screen=Screen()
+# screen.exitonclick()
+
+# from random import *
+# list=[1,2,3,4,5,6]
+# computerchoice=choice(list)
+# print(computerchoice)
+
+""" Giving Modules an Alias name """
+import turtle as t
+ 
