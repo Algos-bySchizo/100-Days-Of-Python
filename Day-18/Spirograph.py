df530da1866584d3c6764d3b75999214e3ef3cd4
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-06-28 04:40:11 +0500

    Applied the previous knowledge to make a Spirograph

---
+++ Day-18/Spirograph.py
@@ -0,0 +1,17 @@
+from turtle import Turtle,Screen
+import random
+tit=Turtle()
+screen=Screen()
+screen.colormode(255)
+def random_color():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    color_tuple=(r,g,b)
+    return color_tuple
+tit.speed('fastest')
+for i in range (72):
+    tit.color(random_color())
+    tit.circle(100)
+    tit.right(5)
+screen.exitonclick()
