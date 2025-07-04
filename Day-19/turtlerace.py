5e7bbb92a9a57a8c46a4ac5bdf2056d1fbb6fa51
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-07-04 21:07:17 +0500

    implemented the knowledge of turtle.goto(x-int,y=int) and turtle.textinput(title=str, prompt=str) in the Turtle Racing Game

--- Day-19/turtlerace.py
+++ Day-19/turtlerace.py
@@ -1,12 +1,11 @@
 from turtle import Turtle,Screen
 import random
-colors_list= ['red', 'blue', 'green', 'orange', 'purple', 'pink']
 screen=Screen()
 screen.colormode(255)
 screen.setup(width=500,height=400)
 start_y= -100
+colors_list= ['red', 'blue', 'green', 'orange', 'purple', 'pink']
 users_bet=screen.textinput('Place a bet','Enter the turtle you think will win!') 
-screen.listen()
 turtle_list=[]
 for i in range(6):
     t=Turtle(shape='turtle')
@@ -14,4 +13,18 @@ for i in range(6):
     t.penup()
     t.goto(x=-230, y=start_y + i * 40)
     turtle_list.append(t)
+is_race_on=False
+if users_bet:
+    is_race_on=True
+    while is_race_on:
+        for turtle in turtle_list:
+            if turtle.xcor()>230:
+                is_race_on=False
+                winning_color=turtle.pencolor()
+                if winning_color==users_bet:
+                    print(f'You\'ve won the bet, The {winning_color} Turtle is the winner!')
+                else:
+                    print(f"you've lost the bet,  The {winning_color} Turtle is the winner!")
+            pace=random.randint(0,10)
+            turtle.forward(pace)
 screen.exitonclick()
