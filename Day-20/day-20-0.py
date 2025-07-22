8c09114ca780c6d8c0b19181fc2b35acf19e5bf7
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-07-22 17:29:14 +0500

    Implemented the knowledge of tracer and update

--- Day-20/day-20-0.py
+++ Day-20/day-20-0.py
@@ -1,12 +1,22 @@
 from turtle import Turtle,Screen,Shape
-starting_positions=[(0,0),(-20,0),(-40,0)]
-for position in starting_positions:
-    snake=Turtle('square')
-    snake.color('white')
-    snake.goto(position)
-
+import time
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor('black')
 screen.title('Snake Xenxia')
+screen.tracer(0)
+starting_positions=[(0,0),(-20,0),(-40,0)]
+snakes=[]
+for position in starting_positions:
+    snake=Turtle('square')
+    snake.color('white')
+    snake.penup()
+    snake.goto(position)
+    snakes.append(snake)
+game_is_on=True 
+while game_is_on:
+    screen.update()
+    time.sleep(0.03)
+    for snake in snakes:
+        snake.forward(20)
 screen.exitonclick()
