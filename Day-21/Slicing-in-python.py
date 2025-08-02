926f07cb1e8c5220eb5e9cceac23aa7659253162
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-08-02 22:25:57 +0500

    Learned and implemented Slicing of lists in Python

---
+++ Day-21/Slicing-in-python.py
@@ -0,0 +1,19 @@
+""" Slicing a list in python """
+
+list=['a','b','c','d','e']
+print(list[2:4])
+
+""" Slicing from an index to the end """
+
+list=['a','b','c','d','e']
+print(list[2:])
+
+""" Slicing from the start to a specific index """
+
+list=['a','b','c','d','e']
+print(list[:4])
+
+""" Increment by a number while slicing between a range of items in a list """
+
+list=['a','b','c','d','e','f','g','h']
+print(list[1:6:2])
