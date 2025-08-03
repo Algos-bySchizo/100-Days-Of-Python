c26d9aa2a98bd891e126c0b1f6a078b6ff8d78b6
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-08-03 14:32:19 +0500

    Created a Paddle using turtle object and implemented the screen.listen() function to make it goo up or down by 20 paces

--- Day-22/main.py
+++ Day-22/main.py
@@ -1,6 +1,23 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+# from paddle import Paddle
 screen=Screen()
 screen.title('Ping Pong')
 screen.bgcolor('black')
 screen.setup(800,600)
-screen.exitonclick()   
+pad1=Turtle()
+pad1.shape('square')
+pad1.color('white')
+pad1.shapesize(5,1)
+pad1.penup()
+pad1.goto(350,0)
+def go_up():
+    new_y=pad1.ycor()+20
+    pad1.goto(350,new_y)
+def go_down():
+    new_y=pad1.ycor()-20
+    pad1.goto(350,new_y)
+
+screen.listen()
+screen.onkey(go_up,'Up')
+screen.onkey(go_down,'Down')
+screen.exitonclick()
---
+++ Day-22/paddle.py
@@ -0,0 +1,9 @@
+# # from turtle import Turtle
+# STARTING_POS=[(350,0),(-350,0)]
+# class Paddle(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.shape('square')
+#         self.color('white')
+#         self.shapesize()
+#         self
