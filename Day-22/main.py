adbdad8056a87b21f0e98f32d67f349b9bf4d2de
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-08-05 04:34:05 +0500

    Detect ball collisions with walls and paddles update direction accordingly

--- Day-22/main.py
+++ Day-22/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 screen=Screen()
 screen.title('Ping Pong')
 screen.bgcolor('black')
@@ -8,6 +10,7 @@ screen.tracer(0)
 
 r_paddle=Paddle((350,0))
 l_paddle=Paddle((-350,0))
+ball=Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up,'Up')
@@ -17,6 +20,14 @@ screen.onkey(l_paddle.go_down,'s')
 
 game_is_on=True
 while game_is_on:
+    time.sleep(0.05)
     screen.update()
+    ball.move()
+    if ball.ycor()==300 or ball.ycor()==-300:
+        ball.bounce()
+    if ball.distance(r_paddle)<70 and ball.xcor()==350 or ball.distance(l_paddle)<70 and ball.xcor()==-350:
+        ball.deflect()
+    if ball.xcor()>380 or ball.xcor()<-380:
+        ball.restartpos()
 
 screen.exitonclick()
---
+++ Day-22/scoreboard.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.l_score=0
+        self.r_score=0
+        
+            
