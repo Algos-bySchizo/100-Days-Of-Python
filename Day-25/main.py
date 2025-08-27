ae6cdd0250f748cf9ecc50657265f449557d8498
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-08-27 13:28:15 +0500

    updates

--- Day-25/main.py
+++ Day-25/main.py
@@ -1,3 +1,17 @@
-with open('weather_data.csv') as data:
-    data=[data.strip() for data in data.readlines()]
-    print(data)
+import csv
+import pandas
+# with open('weather_data.csv') as data:
+#     data=csv.reader(data)
+#     # CSV library is a built-in python library that helps the programmers with CSV file handling and manipulation 
+#     tempratures=[] 
+#     for row in data:
+#         try:
+#             tempratures.append(int(row[1]))
+#         except:
+#             pass
+#         # print(row)
+#     print(tempratures)
+    # data=[data.strip() for data in data.readlines()]    
+    # print(data)
+data=pandas.read_csv('weather_data.csv')
+print(data['temp']) #to get the hold of all the tempratures through out the week using pandas you just have to write data['temp']
